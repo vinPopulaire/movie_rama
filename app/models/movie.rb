fbcce20fe2500bfc1f5fc4b5e1d1477e4a59a600
vinpopulaire <vinpopulaire@gmail.com>
2024-09-07 20:15:19 +0300

    Add validations to Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :user, presence: true
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,4 +2,8 @@ require "rails_helper"
 
 RSpec.describe Movie, type: :model do
   it { should belong_to(:user) }
+
+  it { is_expected.to validate_presence_of :title }
+  it { is_expected.to validate_presence_of :description }
+  it { is_expected.to validate_presence_of :user }
 end
