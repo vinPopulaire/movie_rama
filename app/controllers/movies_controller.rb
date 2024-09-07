9c18327d4141180d1b026f0ca2929a2af4e4abb3
vinpopulaire <vinpopulaire@gmail.com>
2024-09-07 19:23:57 +0300

    Add basic CRUD methods to MoviesController

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,50 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [ :show, :edit, :update, :destroy ]
+
   def index
-    render
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params.merge(user: current_user))
+
+    if @movie.save
+      redirect_to movies_path, notice: "Movie was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movies_path, notice: "Movie was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path, notice: "Movie was successfully destroyed."
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit([ :title, :description ])
   end
 end
---
+++ spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,141 @@
+require 'rails_helper'
+
+RSpec.describe MoviesController, type: :controller do
+  describe 'GET index' do
+    it 'returns a successful response' do
+      get :index
+      expect(response).to be_successful
+    end
+
+    it 'assigns @movies' do
+      movie = FactoryBot.create(:movie)
+      movie2 = FactoryBot.create(:movie)
+
+      get :index
+      expect(assigns(:movies)).to match_array([ movie, movie2 ])
+    end
+
+    it 'renders the index template' do
+      get :index
+
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'GET new' do
+    it 'assigns @movie' do
+      get :new
+      expect(assigns(:movie)).to be_a_new(Movie)
+    end
+
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template('new')
+    end
+  end
+
+  describe 'POST create' do
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      allow(controller).to receive(:current_user).and_return(user)
+    end
+
+    context 'with valid attributes' do
+      let(:valid_attributes) { { title: 'foo', description: 'bar' } }
+
+      it 'creates a new movie and assigns it to @movie' do
+        expect {
+          post :create, params: { movie: valid_attributes }
+        }.to change(Movie, :count).by(1)
+
+        expect(assigns(:movie)).to be_a(Movie)
+        expect(assigns(:movie)).to be_persisted
+      end
+
+      it 'redirects to the movies index with a success notice' do
+        post :create, params: { movie: valid_attributes }
+
+        expect(response).to redirect_to(movies_path)
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:invalid_attributes) { { title: '', description: 'bar' } }
+
+      it 'does not create a new movie' do
+        expect {
+          post :create, params: { movie: invalid_attributes }
+        }.to_not change(Movie, :count)
+      end
+
+      it 'renders the new template with an error flash' do
+        post :create, params: { movie: invalid_attributes }
+
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'GET edit' do
+    let(:movie) { FactoryBot.create(:movie) }
+
+    it 'assigns the requested movie to @movie' do
+      get :edit, params: { id: movie.id }
+      expect(assigns(:movie)).to eq(movie)
+    end
+
+    it 'renders the edit template' do
+      get :edit, params: { id: movie.id }
+      expect(response).to render_template('edit')
+    end
+  end
+
+  describe 'PATCH update' do
+    let(:movie) { FactoryBot.create(:movie) }
+    let(:user) { FactoryBot.create(:user) }
+
+    before do
+      allow(controller).to receive(:current_user).and_return(user)
+    end
+
+    context 'with valid attributes' do
+      let(:valid_attributes) { { title: 'foo', description: 'bar' } }
+
+      it 'updates the movie and redirects to the movies index with a success notice' do
+        patch :update, params: { id: movie.id, movie: valid_attributes }
+
+        movie.reload
+        expect(movie.title).to eq('foo')
+        expect(movie.description).to eq('bar')
+        expect(response).to redirect_to(movies_path)
+        expect(flash[:notice]).to eq('Movie was successfully updated.')
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:invalid_attributes) { { title: '', description: 'Updated description' } }
+
+      it 'does not update the movie and re-renders the edit template' do
+        patch :update, params: { id: movie.id, movie: invalid_attributes }
+
+        movie.reload
+        expect(movie.title).not_to be_empty
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+
+  describe 'DELETE destroy' do
+    let!(:movie) { FactoryBot.create(:movie) }
+
+    it 'deletes the movie and redirects to the movies index with a success notice' do
+      expect {
+        delete :destroy, params: { id: movie.id }
+      }.to change(Movie, :count).by(-1)
+
+      expect(response).to redirect_to(movies_path)
+      expect(flash[:notice]).to eq('Movie was successfully destroyed.')
+    end
+  end
+end
