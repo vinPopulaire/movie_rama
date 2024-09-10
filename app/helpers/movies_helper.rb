262fb2a844deba03352c39fd5f1d51b721d7ad5c
vinpopulaire <vinpopulaire@gmail.com>
2024-09-10 21:52:49 +0300

    Add be the first to vote for this movie prompt in movies index

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,32 +1,16 @@
 module MoviesHelper
-  def relevant_preferences_like_link(movie, user_movie_preferences)
+  def relevant_preferences_link(movie, user_movie_preferences, message, action)
     if (preference = user_movie_preferences[movie.id]).present?
-      if preference.action == "like"
-        movie.like_count.to_s + " likes"
+      if preference.action.to_sym == action
+        message
       else
-        link_to movie.like_count.to_s + " likes",
-                user_movie_preference_path(id: preference.id, movie_id: movie.id, user_action: :like),
+        link_to message,
+                user_movie_preference_path(id: preference.id, movie_id: movie.id, user_action: action),
                 data: { turbo_method: :put }
       end
     else
-      link_to movie.like_count.to_s + " likes",
-              user_movie_preferences_path(movie_id: movie.id, user_action: :like),
-              data: { turbo_method: :post }
-    end
-  end
-
-  def relevant_preferences_hate_link(movie, user_movie_preferences)
-    if (preference = user_movie_preferences[movie.id]).present?
-      if preference.action == "hate"
-        movie.hate_count.to_s + " hates"
-      else
-        link_to movie.hate_count.to_s + " hates",
-                user_movie_preference_path(id: preference.id, movie_id: movie.id, user_action: :hate),
-                data: { turbo_method: :put }
-      end
-    else
-      link_to movie.hate_count.to_s + " hates",
-              user_movie_preferences_path(movie_id: movie.id, user_action: :hate),
+      link_to message,
+              user_movie_preferences_path(movie_id: movie.id, user_action: action),
               data: { turbo_method: :post }
     end
   end
